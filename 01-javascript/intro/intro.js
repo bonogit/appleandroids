0f2168c6ec129850280f88a207089265105b5e88
bono <bono.lau@gmail.com>
2015-09-28 15:33:30 +1000

    the first js

---
+++ 01-javascript/intro/intro.js
@@ -0,0 +1,9 @@
+console.log('first program');
+var numberOfChildren = 9;
+var partnerName = 'Donald';
+var geographiclocation = 'Sunshine';
+var jobTitle = ' pet food taster';
+
+var fortune = 'You will be a ' + jobTitle + ' in '+ geographiclocation + ' , and married to '+ partnerName + ' with ' + numberOfChildren + ' children ';
+
+console.log(fortune)
